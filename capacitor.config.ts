fcf5f31adf6ea07c44d1de613d62d5d5e7fd1813
Karol Diaz <karol.jesusdiaz@gmail.com>
2022-03-30 11:41:47 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'prueba git',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
